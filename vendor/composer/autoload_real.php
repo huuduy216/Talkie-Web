6d110b84ed9f8cd8d1cdaf51c831708245ce2519
Tung Pham <joseph.pham280996@gmail.com>
2020-12-18 08:40:52 -0500

    Tal 15 swap suggestion and navigation bar side (#35)
    * TAL-15 Add CSS to restyling
    
    * TAL-15 Delete mistaken class
    
    * TAL-15 Add Always Show Suggestion List Extension
    
    * Update gitignore
    
    * TAL-15 Fix pinned posts
    
    * Remove node_modules in
    
    * Remove node_modules and update composer.json
    
    * Update packages
    
    * Fix Rebase conflict less/forum.less

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInit88ee21944a6489eec8a793ac5fe51baa
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit88ee21944a6489eec8a793ac5fe51baa', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit88ee21944a6489eec8a793ac5fe51baa', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit88ee21944a6489eec8a793ac5fe51baa::getInitializer($loader));
         } else {
