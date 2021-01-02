e1659acf89eecd4756bbbbc8558c4f222d61dcd7
PhuTan <56894648+PhuTan-Le@users.noreply.github.com>
2021-01-02 14:32:56 +0700

    TAL-84 screen rotation (#56)

--- vendor/askvortsov/flarum-pwa/src/PWATrait.php
+++ vendor/askvortsov/flarum-pwa/src/PWATrait.php
@@ -34,7 +34,7 @@ trait PWATrait
             'theme_color'      => $this->settings->get('theme_primary_color'),
             'background_color' => $this->settings->get('askvortsov-pwa.backgroundColor', '#ffffff'),
             'display'          => 'standalone',
-            'orientation'      => 'any',
+            'orientation'      => 'auto',    
             'icons'            => [],
         ];
 
