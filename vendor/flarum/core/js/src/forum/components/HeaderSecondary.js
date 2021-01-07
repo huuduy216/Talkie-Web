aad7e9d739a1189acfde25aa50ae75d3f91a060e
Dung <01667117609t@gmail.com>
2021-01-07 16:48:28 +0700

    TAL-74 Add animation to registration button

--- vendor/flarum/core/js/src/forum/components/HeaderSecondary.js
+++ vendor/flarum/core/js/src/forum/components/HeaderSecondary.js
@@ -30,6 +30,38 @@ export default class HeaderSecondary extends Component {
    *
    * @return {ItemList}
    */
+  animateSignUp(){
+    const svg = `<svg class="item-signUp" type="button" title="Đăng ký" viewBox="-1 0 50 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect width="46" height="17" fill="#E5E5E5"/>
+    <g id="Frame1">
+    <rect width="46" height="17" fill="white"/>
+    <text id="dd" dominant-baseline="text-after-edge" fill="#667C99" xml:space="preserve" style="white-space: pre" font-family="system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Ubuntu,Cantarell,Oxygen,Roboto,Helvetica,Arial,sans-serif" font-size="13" letter-spacing="0em"><tspan x="-1" y="13.1016">&#x110;</tspan></text>
+    <text id="aw" dominant-baseline="text-after-edge" fill="#667C99" xml:space="preserve" style="white-space: pre" font-family="system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Ubuntu,Cantarell,Oxygen,Roboto,Helvetica,Arial,sans-serif" font-size="13" letter-spacing="0em"><tspan x="8" y="13.1016">&#x103;</tspan></text>
+    <text id="n" dominant-baseline="text-after-edge" fill="#667C99" xml:space="preserve" style="white-space: pre" font-family="system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Ubuntu,Cantarell,Oxygen,Roboto,Helvetica,Arial,sans-serif" font-size="13" letter-spacing="0em"><tspan x="15" y="13.1016">n</tspan></text>
+    <text id="g" dominant-baseline="text-after-edge" fill="#667C99" xml:space="preserve" style="white-space: pre" font-family="system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Ubuntu,Cantarell,Oxygen,Roboto,Helvetica,Arial,sans-serif" font-size="13" letter-spacing="0em"><tspan x="22" y="13.1016">g</tspan></text>
+    <text id="k" dominant-baseline="text-after-edge" fill="#667C99" xml:space="preserve" style="white-space: pre" font-family="system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Ubuntu,Cantarell,Oxygen,Roboto,Helvetica,Arial,sans-serif" font-size="13" letter-spacing="0em"><tspan x="32" y="13.1016">k</tspan></text>
+    <text id="ys" dominant-baseline="text-after-edge" fill="#667C99" xml:space="preserve" style="white-space: pre" font-family="system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Ubuntu,Cantarell,Oxygen,Roboto,Helvetica,Arial,sans-serif" font-size="13" letter-spacing="0em"><tspan x="39" y="13.1016">&#xfd;</tspan></text>
+    </g>
+    </svg>`;
+    return m('div',[m.trust(svg)])
+  }
+  animateLogIn(){
+    const svg = `<svg class="item-logIn" type="button" title="Đăng nhập" viewBox="0 -1 61 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect width="61" height="18" fill="#E5E5E5"/>
+    <g id="Frame2">
+    <rect width="61" height="18" fill="white"/>
+    <text id="n_3" dominant-baseline="text-after-edge" fill="#667C99" xml:space="preserve" style="white-space: pre" font-family="system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Ubuntu,Cantarell,Oxygen,Roboto,Helvetica,Arial,sans-serif" font-size="13" letter-spacing="0em"><tspan x="33" y="13.1016">n</tspan></text>
+    <text id="h" dominant-baseline="text-after-edge" fill="#667C99" xml:space="preserve" style="white-space: pre" font-family="system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Ubuntu,Cantarell,Oxygen,Roboto,Helvetica,Arial,sans-serif" font-size="13" letter-spacing="0em"><tspan x="40" y="13.1016">h</tspan></text>
+    <text id="aaj" dominant-baseline="text-after-edge" fill="#667C99" xml:space="preserve" style="white-space: pre" font-family="system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Ubuntu,Cantarell,Oxygen,Roboto,Helvetica,Arial,sans-serif" font-size="13" letter-spacing="0em"><tspan x="47" y="13.1016">&#x1ead;</tspan></text>
+    <text id="p" dominant-baseline="text-after-edge" fill="#667C99" xml:space="preserve" style="white-space: pre" font-family="system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Ubuntu,Cantarell,Oxygen,Roboto,Helvetica,Arial,sans-serif" font-size="13" letter-spacing="0em"><tspan x="54" y="13.1016">p</tspan></text>
+    <text id="aw_2" dominant-baseline="text-after-edge" fill="#667C99" xml:space="preserve" style="white-space: pre" font-family="system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Ubuntu,Cantarell,Oxygen,Roboto,Helvetica,Arial,sans-serif" font-size="13" letter-spacing="0em"><tspan x="9" y="13.1016">&#x103;</tspan></text>
+    <text id="n_2" dominant-baseline="text-after-edge" fill="#667C99" xml:space="preserve" style="white-space: pre" font-family="system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Ubuntu,Cantarell,Oxygen,Roboto,Helvetica,Arial,sans-serif" font-size="13" letter-spacing="0em"><tspan x="16" y="13.1016">n</tspan></text>
+    <text id="g_2" dominant-baseline="text-after-edge" fill="#667C99" xml:space="preserve" style="white-space: pre" font-family="system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Ubuntu,Cantarell,Oxygen,Roboto,Helvetica,Arial,sans-serif" font-size="13" letter-spacing="0em"><tspan x="23" y="13.1016">g</tspan></text>
+    <text id="dd_2" dominant-baseline="text-after-edge" fill="#667C99" xml:space="preserve" style="white-space: pre" font-family="system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Ubuntu,Cantarell,Oxygen,Roboto,Helvetica,Arial,sans-serif" font-size="13" letter-spacing="0em"><tspan x="0" y="13.1016">&#x110;</tspan></text>
+    </g>
+    </svg>`;
+    return m('div',[m.trust(svg)])
+  }
   items() {
     const items = new ItemList();
 
@@ -74,7 +106,7 @@ export default class HeaderSecondary extends Component {
         items.add(
           'signUp',
           Button.component({
-            children: app.translator.trans('core.forum.header.sign_up_link'),
+            children: this.animateSignUp(),
             className: 'Button Button--link',
             onclick: () => app.modal.show(new SignUpModal()),
           }),
@@ -85,7 +117,7 @@ export default class HeaderSecondary extends Component {
       items.add(
         'logIn',
         Button.component({
-          children: app.translator.trans('core.forum.header.log_in_link'),
+          children: this.animateLogIn(),
           className: 'Button Button--link',
           onclick: () => app.modal.show(new LogInModal()),
         }),
