e6d018c2efa52444af36039ece5734c469b6c594
Tung Pham <joseph.pham280996@gmail.com>
2021-01-25 18:01:04 -0500

    Fix checking for class name injection

---
+++ workbench/HideDiscussionListMainPage/js/src/forum/dist/index.dev.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var _app = _interopRequireDefault(require("flarum/app"));
+
+var _extend = require("flarum/extend");
+
+var _HeaderPrimary = _interopRequireDefault(require("flarum/components/HeaderPrimary"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+_app["default"].initializers.add("joseph-always-show-suggestion", function () {
+  (0, _extend.extend)(_HeaderPrimary["default"].prototype, "config", function (isInitialized, context) {
+    if (isInitialized) return;
+    var contentContainer = document.getElementsByClassName("App-content")[0];
+
+    if (window.location.pathname === "/" || window.location.pathname.includes("public/")) {
+      contentContainer.classList.add("mr-0");
+    } else {
+      contentContainer.classList.remove("mr-0");
+    }
+  });
+});
--- workbench/HideDiscussionListMainPage/js/src/forum/index.js
+++ workbench/HideDiscussionListMainPage/js/src/forum/index.js
@@ -1,15 +1,18 @@
-import app from 'flarum/app';
-import {extend} from 'flarum/extend'
-import HeaderPrimary from 'flarum/components/HeaderPrimary'
+import app from "flarum/app";
+import { extend } from "flarum/extend";
+import HeaderPrimary from "flarum/components/HeaderPrimary";
 
-app.initializers.add('joseph-always-show-suggestion', function () {
-  extend(HeaderPrimary.prototype, 'config', function(isInitialized, context){
+app.initializers.add("joseph-always-show-suggestion", function () {
+  extend(HeaderPrimary.prototype, "config", function (isInitialized, context) {
     if (isInitialized) return;
-      const contentContainer = document.getElementsByClassName('App-content')[0]
-    if (window.location.pathname === '/' || window.location.pathname === '/flarum/public/'){
-      contentContainer.classList.add('mr-0')
+    const contentContainer = document.getElementsByClassName("App-content")[0];
+    if (
+      window.location.pathname === "/" ||
+      window.location.pathname.includes("public/")
+    ) {
+      contentContainer.classList.add("mr-0");
     } else {
-      contentContainer.classList.remove('mr-0')
+      contentContainer.classList.remove("mr-0");
     }
-  })
+  });
 });
